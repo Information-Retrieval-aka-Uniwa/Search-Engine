0a91118419ce93994c1f8a5af8401f7d77b6c0eb
PanthegrammerPRO <pantelis.t24@gmail.com>
2024-01-17 23:46:34 +0200

    kokokoko

--- retrieval_algos.py
+++ retrieval_algos.py
@@ -6,15 +6,15 @@ def search_papers_boolean(query, inverted_index):
 
     for i, term in enumerate(terms):
 
-        if term == "and" and i > 0 and i < len(terms) - 1:
-            previous_term = terms[i - 1]
-            next_term = terms[i + 1]
-            if previous_term and next_term in inverted_index:
-                documents_with_previous_term = inverted_index[previous_term]
-                documents_with_next_term = inverted_index[next_term]
+        if term == "and" and i > 0 and i < len(terms) - 1:             # Έλεγχος για το αν ο όρος κλειδί (term) είναι ο "and" και αν βρίσκεται μεταξύ δύο όρων κλειδιών (terms)
+            previous_term = terms[i - 1]                               # Ο προηγούμενος όρος κλειδί (term) από τον όρο κλειδί (term) που εξετάζουμε
+            next_term = terms[i + 1]                                   # Ο επόμενος όρος κλειδί (term) από τον όρο κλειδί (term) που εξετάζουμε
+            if previous_term and next_term in inverted_index:                 # Ο προηγούμενος και ο επόμενος όρος κλειδί (term) υπάρχουν στο ευρετήριο
+                documents_with_previous_term = inverted_index[previous_term]  # Ανάκτηση των αριθμών των εργασιών που περιέχουν τον προηγούμενο όρο κλειδί (term)
+                documents_with_next_term = inverted_index[next_term]          # Ανάκτηση των αριθμών των εργασιών που περιέχουν τον επόμενο όρο κλειδί (term)
                 
                 for document in documents_with_previous_term:
-                    if document in documents_with_next_term:
+                    for document in documents_with_next_term:
                         matching_papers.append(document)
 
         elif term == "or" and i > 0 and i < len(terms) - 1:
@@ -25,11 +25,7 @@ def search_papers_boolean(query, inverted_index):
                 documents_with_next_term = inverted_index[next_term]
                 
                 for document in documents_with_previous_term:
-                    if document in documents_with_next_term:
-                        matching_papers.append(document)
-                
-                for document in documents_with_next_term:
-                    if document in documents_with_previous_term:
+                    for document in documents_with_next_term:
                         matching_papers.append(document)
 
         elif term == "not" and i < len(terms) - 1:
@@ -39,9 +35,8 @@ def search_papers_boolean(query, inverted_index):
                 for document in documents:           
                     matching_papers.append(document) 
 
-    matching_papers = set(matching_papers) 
     
-    return matching_papers
+    return set(matching_papers) # Αφαίρεση των διπλότυπων αριθμών εργασιών που περιέχουν το ερώτημα αναζήτησης
 
 
 def search_papers_default(query, inverted_index):
