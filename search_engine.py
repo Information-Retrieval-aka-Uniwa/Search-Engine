7402b9123b20557187a0accda3ca8855672bc1b0
PanthegrammerPRO <pantelis.t24@gmail.com>
2024-01-17 22:32:10 +0200

    yo

--- search_engine.py
+++ search_engine.py
@@ -58,9 +58,9 @@ def print_papers(algo,search_query, papers, inverted_index):
     returned_docs = [] # Αρχικοποίηση της λίστας με τα αποτελέσματα της αναζήτησης
 
     if algo == "Boolean Retrieval":
-        returned_docs = search_papers_bool(search_query, inverted_index)
+        returned_docs = search_papers_boolean(search_query, inverted_index)
     else:    
-        returned_docs = search_papers(search_query, inverted_index) # Κλήση της συνάρτησης search_papers για την αναζήτηση των εργασιών που περιέχουν το ερώτημα αναζήτησης
+        returned_docs = search_papers_default(search_query, inverted_index) # Κλήση της συνάρτησης search_papers για την αναζήτηση των εργασιών που περιέχουν το ερώτημα αναζήτησης
 
     print("Οι εργασίες που περιέχουν το ερώτημα αναζήτησης είναι: \n")
     for doc in returned_docs:                                # Προσπέλαση της λίστας με τα αποτελέσματα της αναζήτησης
@@ -72,34 +72,6 @@ def print_papers(algo,search_query, papers, inverted_index):
         print("Abstract:", paper.get("abstract"))            # Εκτύπωση της περίληψης της εργασίας
         print("Date:", paper.get("date"))                    # Εκτύπωση της ημερομηνίας δημοσίευσης της εργασίας
         print("PDF URL:", paper.get("pdf_url"), "\n\n")      # Εκτύπωση του συνδέσμου για τη λήψη του PDF της εργασίας
-
-
-"""
-Βήμα 4. Μηχανή αναζήτησης (Search engine)
-
-def search_papers(query, inverted_index)
-
-Είσοδος[1] --> [query]            Ερώτημα αναζήτησης που εισήγαγε ο χρήστης στην διεπαφή χρήστη
-Είσοδος[2] --> [inverted_index]   Ανεστραμμένη δομή δεδομένων ευρετηρίου αποθηκευμένη σε μία δομή λεξικού 
-Λειτουργία -->                    Αναζήτηση των εργασιών που περιέχουν το ερώτημα αναζήτησης
-Έξοδος     --> [matching_papers]  Λίστα με τους αριθμούς των εργασιών που περιέχουν το ερώτημα αναζήτησης
-    
-"""
-def search_papers(query, inverted_index):
-    
-    terms = query.lower().split()                # Μετατροπή του ερωτήματος αναζήτησης σε πεζά και διαχωρισμός του σε λεκτικές μονάδες
-    matching_papers = []                         # Αρχικοποίηση της λίστας με τους αριθμούς των εργασιών που περιέχουν το ερώτημα αναζήτησης
-
-    for term in terms:                           # Προσπέλαση των λεκτικών μονάδων του ερωτήματος αναζήτησης
-        if term in inverted_index:               # Ο όρος κλειδί (term) υπάρχει στο ευρετήριο
-            documents = inverted_index[term]     # Ανάκτηση των αριθμών των εργασιών που περιέχουν τον όρο κλειδί (term)
-            for document in documents:           # Προσπέλαση των αριθμών των εργασιών που περιέχουν τον όρο κλειδί (term)
-                matching_papers.append(document) # Προσθήκη του αριθμού της εργασίας στη λίστα με τους αριθμούς των εργασιών που περιέχουν το ερώτημα αναζήτησης
-
-    matching_papers = set(matching_papers)       # Αφαίρεση των διπλότυπων αριθμών εργασιών που περιέχουν το ερώτημα αναζήτησης
-    
-    return matching_papers                       # Επιστροφή της λίστας με τους αριθμούς των εργασιών που περιέχουν το ερώτημα αναζήτησης
-
 
 
 
