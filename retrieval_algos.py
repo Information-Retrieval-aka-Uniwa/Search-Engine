ad8fb2dd14e54f7453d691dc9d7284ad9f0e1946
Tiger <billath131908@gmail.com>
2024-01-18 05:50:00 +0200

    first commit

--- retrieval_algos.py
+++ retrieval_algos.py
@@ -1,8 +1,6 @@
 def search_papers_boolean(query, inverted_index):
-    
     terms = query.lower().split()  # Μετατροπή του ερωτήματος αναζήτησης σε πεζά γράμματα και διαχωρισμός του σε λεκτικές μονάδες
     matching_papers = []           # Αρχικοποίηση της λίστας με τους αριθμούς των εργασιών που περιέχουν το ερώτημα αναζήτησης              
-    
     for i, term in enumerate(terms):
 
         if term == "and" and i > 0 and i < len(terms) - 1:             # Έλεγχος για το αν ο όρος κλειδί (term) είναι ο "and" και αν βρίσκεται μεταξύ δύο όρων κλειδιών (terms)
@@ -31,7 +29,7 @@ def search_papers_boolean(query, inverted_index):
             next_term = terms[i + 1]
             if next_term not in inverted_index:
                 documents_without_next_term = inverted_index[next_term]    
-                for document in documents_with_previous_term:           
+                for document in documents_without_next_term:           
                     matching_papers.append(document) 
 
     
@@ -50,4 +48,4 @@ def search_papers_default(query, inverted_index):
                 matching_papers.append(document) # Προσθήκη του αριθμού της εργασίας στη λίστα με τους αριθμούς των εργασιών που περιέχουν το ερώτημα αναζήτησης
 
     
-    return set(matching_papers)                       # Αφαίρεση των διπλότυπων αριθμών εργασιών που περιέχουν το ερώτημα αναζήτησης
+    return set(matching_papers)                  # Αφαίρεση των διπλότυπων αριθμών εργασιών που περιέχουν το ερώτημα αναζήτησης
